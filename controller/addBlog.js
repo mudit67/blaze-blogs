d0ad45fcb8bab1b2aa80cb468a6a1884c08e4ff2
mudit67 <bhardwajmudit006@gmail.com>
2022-03-14 14:19:55 +0530

    addblog error handling

--- controller/addBlog.js
+++ controller/addBlog.js
@@ -4,18 +4,15 @@ module.exports = async function (req,res,next) {
     console.log(req.body);
     const {title,author, content,postDate} = req.body;
     const createBlog = new Blog({
-        title:title,
-        author:author,
-        content:content,
-        postDate:postDate
+        title,
+        author,
+        content,
+        postDate
     });
     try{
         await createBlog.save();
     } catch(err) {
-        // const error = new HttpError('blog post failed', 500);
-        // return next(err);
-        console.error(err);
-        res.status(500).send({});
+        res.status(400).send(Object.values(err.errors).map(({message}) => (message)));
         return;
     }
     res.status(201).json({ blog: createBlog });
